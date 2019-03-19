92d81fd970d2df699b594ef2eb2ee1b06fb5fce4
ManfredSteyer <manfred.steyer@gmx.net>
2019-03-20 00:28:31 +0100

    init

---
+++ apps/flight-app/src/app/dashboard-tile/dashboard-tile.module.ts
@@ -0,0 +1,22 @@
+import { NgModule, Injector } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { createCustomElement } from '@angular/elements';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { DashboardTileComponent } from './dashboard-tile.component';
+
+@NgModule({
+  imports: [CommonModule, NgxChartsModule],
+  declarations: [DashboardTileComponent],
+  exports: [DashboardTileComponent],
+  entryComponents: [
+    // TODO: Add DashboardTileComponent
+    // DashboardTileComponent
+  ]
+})
+export class DashboardTileModule {
+  constructor(private injector: Injector) {
+    // TODO: Wrap DashboardTileComponent as a web component and register it
+    // const Elm = createCustomElement(DashboardTileComponent, {injector});
+    // customElements.define('dashboard-tile', Elm);
+  }
+}
